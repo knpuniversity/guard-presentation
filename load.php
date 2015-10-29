6ba251b363130cbd93da21d820fdaba355f9d094
Ryan Weaver <ryan@thatsquality.com>
2015-10-29 00:58:10 -0400

    cheap load script

---
+++ load.php
@@ -0,0 +1,18 @@
+<?php
+
+use AppBundle\Entity\User;
+
+$loader = require __DIR__.'/app/autoload.php';
+include_once __DIR__.'/app/bootstrap.php.cache';
+
+$kernel = new AppKernel('dev', true);
+$kernel->boot();
+
+$container = $kernel->getContainer();
+
+$em = $container->get('doctrine.orm.entity_manager');
+
+$user = new User('weaverryan');
+$em->persist($user);
+$em->flush();
+
