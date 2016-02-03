059976912f131a0c0aa9c014d7d98d8ef957e791
Ryan Weaver <ryan@thatsquality.com>
2016-02-03 16:32:12 -0500

    FormLogin: Adding error and redirect

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This is code behind the Guard presentation first given
 at Symfony Live San Francisco in 2015.
 
 The `finish` branch contains the ending code.
+
--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -4,15 +4,18 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends Controller
 {
     /**
      * @Route("/login", name="security_login")
      */
-    public function loginAction()
+    public function loginAction(Request $request)
     {
-        return $this->render('security/login.html.twig');
+        return $this->render('security/login.html.twig', [
+            'error' => $request->getSession()->get('_security.last_error')
+        ]);
     }
 
     /**
